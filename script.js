6dc0dbaf08a9a8248181c438048263050cb5eeb7
Hla Htun <kshap.karki@gmail.com>
2023-01-29 16:02:31 -0800

    setting up the basics

--- script.js
+++
@@ -1,4 +0,0 @@
-let centerDiv = document.getElementById("center-div");
-let windowWidth = window.innerWidth;
-let divWidth = centerDiv.offsetWidth;
-centerDiv.style.left = (windowWidth - divWidth) / 2 + "px";
