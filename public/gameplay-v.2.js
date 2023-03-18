201f480b91571212764fd03b0c798a13b1b8e22d
Hla Htun <kshap.karki@gmail.com>
2023-03-18 14:22:03 -0700

    the keyboard buttons now work, previously was only listening to keydown inputs

--- public/gameplay-v.2.js
+++ public/gameplay-v.2.js
@@ -7,20 +7,25 @@ window.onload = function() {
 
 document.addEventListener('DOMContentLoaded', function() {
     // ********* setting up **************
-    createKeyboard()
-    createInputLabels()
+        createKeyboard()
+        createInputLabels()
     // ***********************************
 
-    let chosenWord = ""
-    const winTextContext = "Well Done!\nWould you like to guess another word?";
+    // ********* variables **************
+    let chosenWord = "";
     let lostTextContext = "";
+    let userWord = "";
     let fillingForm = false;
+    let ignoreInput = false;
+    let ignoreBackspace = false;
+    let currentInputLabelIndex = 0;
+    // ***********************************
 
     // when home button is clicked
     const homeButton = document.getElementById('homeButton')
     const resetButton = document.getElementById('resetButton')
     const feedbackButton = document.getElementById('feedbackButton')
-
+    const labels = document.getElementsByClassName("input-label")
     homeButton.addEventListener('click', goBackHome);
 
     resetButton.addEventListener('click', function() {
@@ -66,138 +71,153 @@ document.addEventListener('DOMContentLoaded', function() {
             lostTextContext = "Oops!\nThe correct word was " + chosenWord + "!\nWould you like to guess another word?";
         })
 
-
-    const labels = document.getElementsByClassName("input-label")
-    let ignoreInput = false;
-    let ignoreBackspace = false;
-    let userWord = "";
-    let currentInputLabelIndex = 0;
-
-    document.addEventListener('keydown',  function (event) {
-        if (isAlpha(event.key) && !ignoreInput && !fillingForm) {
-            ignoreBackspace = false;
-            // updating the input label and the userWord variable
-            for (let j = 0; j < labels.length; j++) {
-                if (labels[j].textContent === "") {
-                    labels[j].classList.add("scale-up-animation");
-                    labels[j].textContent = event.key.toUpperCase();
-
-                    userWord += event.key.toUpperCase();
-                    // keep track of label no.
-                    currentInputLabelIndex = j;
-                    if (j % (5) === 4) {
-                        ignoreInput = true;
-                    }
-                    break;
-                }
+    const keyboardButtons = document.querySelectorAll(".keyboard-key")
+    keyboardButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            if (!ignoreInput && !fillingForm) {
+                [ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord] = updateInputLabel(ignoreBackspace, currentInputLabelIndex, ignoreInput, button.textContent, userWord);
             }
+        });
+    });
+
+    const eraseButton = document.querySelector(".erase-key")
+    eraseButton.addEventListener("click", function() {
+        if (!ignoreBackspace) {
+            [ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord] = eraseLetter(ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord);
+        }
+    })
+
+    const enterButton = document.querySelector(".enter-key")
+    enterButton.addEventListener("click", async function() {
+        if (ignoreInput) {
+            [ignoreBackspace, currentInputLabelIndex , ignoreInput, userWord, lostTextContext, chosenWord] = await processWord(ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord, lostTextContext, chosenWord);
+        }
+    })
+
+    document.addEventListener('keydown',  async function (event) {
+        if (isAlpha(event.key) && !ignoreInput && !fillingForm) {
+            [ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord] = updateInputLabel(ignoreBackspace, currentInputLabelIndex, ignoreInput, event.key, userWord);
+            // console.log(currentInputLabelIndex)
         }
         else if (event.key === "Backspace" && !ignoreBackspace) {
-            // don't let back spacing jump from current attempt to previous attempt
-            if (currentInputLabelIndex % 5 === 0) {
-                ignoreBackspace = true;
-            }
-            labels[currentInputLabelIndex].textContent = "";
-            labels[currentInputLabelIndex].classList.remove("scale-up-animation");
-            userWord = userWord.slice(0, -1);
-            currentInputLabelIndex -= 1;
-            if (currentInputLabelIndex % 5 !== 0) {
-                ignoreInput = false;
-            }
+            [ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord] = eraseLetter(ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord);
+            // console.log(currentInputLabelIndex)
         }
         else if (event.key === "Enter" && ignoreInput) {
-            checkWord(userWord)
-                .then(response => {
-                    if (response) {
-                        ignoreInput = false;
-                        ignoreBackspace = true;
-
-                        if (currentInputLabelIndex === 29) {
-                            [userWord, ignoreInput] = winMessage(labels, currentInputLabelIndex, lostTextContext)
-                            newChosenWord().then( ([newWord, lostText]) => {
-                                chosenWord = newWord;
-                                lostTextContext = lostText;
-                            });
-                        }
-
-                        console.log("chosenWord: ", chosenWord)
-                        console.log("userWord: ", userWord)
-                        // if answer is correct
-                        if (chosenWord === userWord) {
-                            for (let i = 0; i < 5; i++) {
-                                labels[(currentInputLabelIndex-4) + i].style.background = "#52894e"
-                            }
-                            ignoreInput = true;
-                            [userWord, ignoreInput] = winMessage(labels, currentInputLabelIndex, winTextContext);
-                            newChosenWord().then( ([newWord, lostText]) => {
-                                chosenWord = newWord;
-                                lostTextContext = lostText;
-                            });
-
-                        } else {
-                            for (let i = 0; i < userWord.length; i++) {
-                                // setting incorrect answer to black
-                                if (!chosenWord.includes(userWord[i])) {
-                                    labels[(currentInputLabelIndex - 4) + i].style.background = "#3a3a3c"
-                                    colorKeyboard(userWord[i], "#3a3a3c");
-                                }
-                                // setting border to none
-                                labels[(currentInputLabelIndex - 4) + i].style.border = "none";
-
-                                // if correct letter
-                                if (chosenWord[i] === userWord[i]) {
-                                    labels[(currentInputLabelIndex - 4) + i].style.background = "#52894e"
-                                    colorKeyboard(userWord[i], "#52894e");
-                                } else {
-                                    for (let j = 0; j < userWord.length; j++) {
-                                        // if semi-correct
-                                        if (userWord[i] === chosenWord[j]) {
-                                            labels[(currentInputLabelIndex - 4) + i].style.background = "#af9a3b"
-                                            colorKeyboard(userWord[i], "#af9a3b");
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        userWord = "";
-                    } else {
-                        notValidWord();
-                        ignoreBackspace = false;
-                    }
-                })
-                .catch(error => {
-                    console.error(error);
-                    console.error("There was error in checking if this is a valid word. Please refresh and try again :(")
-                })
+            [ignoreBackspace, currentInputLabelIndex , ignoreInput, userWord, lostTextContext, chosenWord] = await processWord(ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord, lostTextContext, chosenWord);
         }
     })
 });
+
+
 // ******************* helper functions **********************
 
-// go back to home page
-function goBackHome() {
-    window.location.href = "/home.html";
+function updateInputLabel(ignoreBackspace, currentInputLabelIndex, ignoreInput, inputKey, userWord) {
+    const labels = document.getElementsByClassName("input-label")
+    ignoreBackspace = false;
+    // updating the input label and the userWord variable
+    for (let j = 0; j < labels.length; j++) {
+        if (labels[j].textContent === "") {
+            labels[j].classList.add("scale-up-animation");
+            labels[j].textContent = inputKey.toUpperCase();
+
+            userWord += inputKey.toUpperCase();
+            // keep track of label no.
+            currentInputLabelIndex = j;
+            if (j % (5) === 4) {
+                ignoreInput = true;
+            }
+            break;
+        }
+    }
+    return [ignoreBackspace, currentInputLabelIndex,ignoreInput, userWord]
 }
 
-// call to server to request a random word
-async function fetchWord(word_length){
-    let chosenWord
-    await fetch("/word", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ word: word_length})
-    })
-        .then(response => response.json())
-        .then(data => {
-            chosenWord = data.responseString
-            console.log("received from server: ", chosenWord)
-            // chosenWord = data.responseString.toLowerCase()
-            // updateArrayW(chosenWord)
-        })
+function eraseLetter(ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord) {
+    const labels = document.getElementsByClassName("input-label")
+    let updatedUserWord = "";
+    // don't let back spacing jump from current attempt to previous attempt
+    if (currentInputLabelIndex % 5 === 0) {
+        ignoreBackspace = true;
+    }
+    labels[currentInputLabelIndex].textContent = "";
+    labels[currentInputLabelIndex].classList.remove("scale-up-animation");
+    updatedUserWord = userWord.slice(0, -1);
+    currentInputLabelIndex -= 1;
+    if (currentInputLabelIndex % 5 !== 0) {
+        ignoreInput = false;
+    }
+    return [ignoreBackspace, currentInputLabelIndex, ignoreInput, updatedUserWord]
+}
 
-    return chosenWord
+async function processWord(ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord, lostTextContext, chosenWord) {
+    const labels = document.getElementsByClassName("input-label")
+    const winTextContext = "Well Done!\nWould you like to guess another word?";
+
+    try {
+        const response = await checkWord(userWord);
+
+        if (response) {
+            ignoreInput = false;
+            ignoreBackspace = true;
+
+            if (currentInputLabelIndex === 29) {
+                [userWord, ignoreInput] = winMessage(labels, currentInputLabelIndex, lostTextContext)
+                const [newWord, lostText] = await newChosenWord();
+                chosenWord = newWord;
+                lostTextContext = lostText;
+            }
+
+            console.log("chosenWord: ", chosenWord)
+            console.log("userWord: ", userWord)
+            // if answer is correct
+            if (chosenWord === userWord) {
+                for (let i = 0; i < 5; i++) {
+                    labels[(currentInputLabelIndex - 4) + i].style.background = "#52894e"
+                }
+                ignoreInput = true;
+                [userWord, ignoreInput] = winMessage(labels, currentInputLabelIndex, winTextContext);
+                const [newWord, lostText] = await newChosenWord();
+                chosenWord = newWord;
+                lostTextContext = lostText;
+            }
+            else {
+                for (let i = 0; i < userWord.length; i++) {
+                    // setting incorrect answer to black
+                    if (!chosenWord.includes(userWord[i])) {
+                        labels[(currentInputLabelIndex - 4) + i].style.background = "#3a3a3c"
+                        colorKeyboard(userWord[i], "#3a3a3c");
+                    }
+                    // setting border to none
+                    labels[(currentInputLabelIndex - 4) + i].style.border = "none";
+
+                    // if correct letter
+                    if (chosenWord[i] === userWord[i]) {
+                        labels[(currentInputLabelIndex - 4) + i].style.background = "#52894e"
+                        colorKeyboard(userWord[i], "#52894e");
+                    } else {
+                        for (let j = 0; j < userWord.length; j++) {
+                            // if semi-correct
+                            if (userWord[i] === chosenWord[j]) {
+                                labels[(currentInputLabelIndex - 4) + i].style.background = "#af9a3b"
+                                colorKeyboard(userWord[i], "#af9a3b");
+                            }
+                        }
+                    }
+                }
+            }
+            userWord = "";
+        } else {
+            notValidWord();
+            ignoreBackspace = false;
+        }
+
+        return [ignoreBackspace, currentInputLabelIndex, ignoreInput, userWord, lostTextContext, chosenWord]
+    }
+    catch(error) {
+        console.error(error);
+        console.error("There was error in checking if this is a valid word. Please refresh and try again :(")
+    }
 }
 
 function createKeyboard() {
@@ -366,8 +386,10 @@ function colorKeyboard(letter, color) {
     const keyboardKeys = document.getElementsByClassName("keyboard-key");
     for ( let k = 0; k < keyboardKeys.length; k++) {
         if (keyboardKeys[k].textContent === letter) {
-            keyboardKeys[k].style.background = color
-            keyboardKeys[k].style.border = "none";
+            if (keyboardKeys[k].style.background !== "#52894e") {
+                keyboardKeys[k].style.background = color
+                keyboardKeys[k].style.border = "none";
+            }
         }
     }
 }
@@ -461,4 +483,28 @@ function createFeedbackForm() {
         // append feedback form to body
         document.body.appendChild(feedbackForm);
     }
+}
+
+// go back to home page
+function goBackHome() {
+    window.location.href = "/home.html";
+}
+
+// call to server to request a random word
+async function fetchWord(word_length){
+    let chosenWord
+    await fetch("/word", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ word: word_length})
+    })
+        .then(response => response.json())
+        .then(data => {
+            chosenWord = data.responseString.toLowerCase()
+            // console.log("received from server: ", chosenWord)
+        })
+
+    return chosenWord
 }
