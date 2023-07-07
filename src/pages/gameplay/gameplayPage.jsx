7a0c65c2f9047d9997c3e0f704f6b3ae2296566c
Hla Htun <kshap.karki@gmail.com>
2023-07-07 13:42:33 -0700

    gameplay page basic set up

--- src/pages/gameplay/gameplayPage.jsx
+++ src/pages/gameplay/gameplayPage.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import './gameplayPage.css'
 import LinkTo from "../../components/linkTo";
+import GameplayNavbarComps from "../../components/gameplay/navbar/gameplay-navbar-comps";
 
 const GameplayPage = () => {
     return (
         <div>
-            <div>Gameplay</div>
+            <GameplayNavbarComps />
             <LinkTo path="/" label="Go back to home" />
         </div>
     )
