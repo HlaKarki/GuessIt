ed1f1570a91c391e40ea3b1088be9bfa317a3a01
Hla Htun <kshap.karki@gmail.com>
2023-07-07 13:06:26 -0700

    basic gameplay page set up

---
+++ src/pages/gameplay/gameplayPage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './gameplayPage.css'
+import LinkTo from "../../components/linkTo";
+
+const GameplayPage = () => {
+    return (
+        <div>
+            <div>Gameplay</div>
+            <LinkTo path="/" label="Go back to home" />
+        </div>
+    )
+}
+
+export default GameplayPage
