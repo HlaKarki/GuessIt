69725a0800740404abc3db5840c9b8a9cdd25985
Hla Htun <kshap.karki@gmail.com>
2023-07-07 23:03:44 -0700

    no span element if label is not provided

--- src/components/linkTo.jsx
+++ src/components/linkTo.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const LinkTo = ( {page, label, className} ) => {
     return (
         <Link to={ page ? "/" + page : "/"} className={ className ? className : "none-NA"}>
-            <span title={`Go to ${label} page`}>{label}</span>
+            {label ? <span title={`Go to ${label} page`}>{label}</span> : null}
         </Link>
     )
 }
