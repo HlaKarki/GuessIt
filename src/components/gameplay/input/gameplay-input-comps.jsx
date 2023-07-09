4f2d6d5f738556aa89bbb2b8b4c87b4e29863a70
Hla Htun <kshap.karki@gmail.com>
2023-07-08 21:56:00 -0700

    new feature: the keyboard keys including the Enter and Backspace key

--- src/components/gameplay/input/gameplay-input-comps.jsx
+++ src/components/gameplay/input/gameplay-input-comps.jsx
@@ -2,18 +2,31 @@ import React from "react"
 import './gameplay-input-comps.css'
 // import LinkTo from "../../components/linkTo";
 
+const MAX_ROW_INDEX = 5
+const MAX_COL_INDEX = 4
+
 const EmptyInputLabel = ({ value, id, row, col }) => <label className="input-label" key={id} id={`input_${row}${col}`}>{value}</label>
 
 const EmptyInputSetUp = () => {
     let rowIndex = 0
-    let colIndex = 0
+    let colIndex = -1
     window.addEventListener('keydown', (event) => {
-        if (isAlpha(event.key)){
+        if (isAlpha(event.key) && (rowIndex <= MAX_ROW_INDEX) && (colIndex <= MAX_COL_INDEX) ){
+            if (colIndex !== 4) {
+                colIndex += 1
+            }
             document.getElementById(`input_${rowIndex}${colIndex}`).textContent = event.key.toUpperCase()
-
-            colIndex = (colIndex + 1) % 5
-            if (colIndex === 0) {
-                rowIndex = (rowIndex + 1) % 6;
+        }
+        else if (event.key === "Enter") {
+            if (colIndex === 4 && (rowIndex !== MAX_ROW_INDEX)) {
+                colIndex = -1
+                rowIndex += 1
+            }
+        }
+        else if (event.key === "Backspace") {
+            if (colIndex >= 0) {
+                document.getElementById(`input_${rowIndex}${colIndex}`).textContent = ""
+                colIndex -= 1
             }
         }
     })
@@ -28,7 +41,6 @@ const EmptyInputSetUp = () => {
                 </div>
             ))}
         </div>
-
     )
 }
 
