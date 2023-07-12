abecad482df3aa8fe5eb0c7b206b0aa3e8be47e9
Hla Htun <kshap.karki@gmail.com>
2023-07-11 20:24:47 -0700

    when the user guesses the correct word, confetti will be thrown across the screen

--- src/components/gameplay/input/gameplay-input-comps.jsx
+++ src/components/gameplay/input/gameplay-input-comps.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import './gameplay-input-comps.css'
 import { GetWord } from "../../getWord";
 import { isValid } from "../../checkWord";
+import confetti from 'canvas-confetti'
 
 const MAX_ROW_INDEX = 5
 const MAX_COL_INDEX = 4
@@ -24,6 +25,7 @@ const EmptyInputSetUp = () => {
             if (colIndex !== 4) {
                 colIndex += 1
                 document.getElementById(`input_${rowIndex}${colIndex}`).textContent = event.key.toUpperCase()
+                document.getElementById(`input_${rowIndex}${colIndex}`).classList.add("scale-up-animation")
             }
         }
         else if (event.key === "Enter") {
@@ -41,22 +43,26 @@ const EmptyInputSetUp = () => {
                                 if (CHOSEN_WORD.word[i] === user_word[i]){
                                     document.getElementById(`input_${rowIndex}${i}`).style.backgroundColor = "#52894e"
                                     document.getElementById(`keyboard_${user_word[i]}`).style.backgroundColor = "#52894e"
+                                    flipThis(user_word, rowIndex, i)
                                 }
                                 else {
                                     // YELLOW: check if the letter is present in the word
                                     if (CHOSEN_WORD.word.includes(user_word[i])){
                                         document.getElementById(`input_${rowIndex}${i}`).style.backgroundColor = "#af9a3b"
                                         document.getElementById(`keyboard_${user_word[i]}`).style.backgroundColor = "#af9a3b"
+                                        flipThis(user_word, rowIndex, i)
                                     }
                                     // GRAY: the letter is not part of the answer
                                     else {
                                         document.getElementById(`input_${rowIndex}${i}`).style.backgroundColor = "#3a3a3c"
                                         document.getElementById(`keyboard_${user_word[i]}`).style.backgroundColor = "#3a3a3c"
+                                        flipThis(user_word, rowIndex, i)
                                     }
                                 }
                             }
                             if (user_word === CHOSEN_WORD.word) {
                                 console.log(`good one mf, you got it right`)
+                                startConfetti()
                             }
                             colIndex = -1
                             rowIndex += 1
@@ -88,8 +94,35 @@ const EmptyInputSetUp = () => {
     )
 }
 
+const startConfetti = () => {
+    let duration = 5 * 1000;
+    let animationEnd = Date.now() + duration;
+    let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    let interval = setInterval(function() {
+        let timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        let particleCount = 50 * (timeLeft / duration);
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 250);
+};
+
 function isAlpha(character) {
     return /^[A-Za-z]$/.test(character);
+}
+
+function flipThis(user_word, rowIndex, i) {
+    document.getElementById(`input_${rowIndex}${i}`).classList.add("flip-animation");
+    document.getElementById(`keyboard_${user_word[i]}`).classList.add("flip-animation");
 }
 
 const GameplayInput = () => {
