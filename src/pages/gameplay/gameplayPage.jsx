eca777149b45577d131c494a1857966e0aeb2b2a
Hla Htun <kshap.karki@gmail.com>
2023-07-11 20:26:38 -0700

    if the user is viewing from phone - specifically iphone 11 and below, the /play page will be scrolled a bit down to keep the full keyboard in view

--- src/pages/gameplay/gameplayPage.jsx
+++ src/pages/gameplay/gameplayPage.jsx
@@ -4,9 +4,18 @@ import './gameplayPage.css'
 import GameplayNavbarComps from "../../components/gameplay/navbar/gameplay-navbar-comps";
 import GameplayKeyboardComps from "../../components/gameplay/keyboard/gameplay-keyboard-comps";
 import GameplayInput from "../../components/gameplay/input/gameplay-input-comps";
+const isMobile = window.innerWidth <= 414;
 
 const GameplayPage = () => {
+    if (isMobile) {
+        const viewportHeight = window.innerHeight;
+        const desiredPosition = viewportHeight - (viewportHeight * 0.95);
 
+        window.scrollTo({
+            top: desiredPosition, // Replace 'desiredPosition' with the vertical position you want to scroll to
+            behavior: 'smooth' // Add smooth scrolling animation, omit this property for instant scrolling
+        });
+    }
     return (
         <div className={"gameplay"}>
             <GameplayNavbarComps />
