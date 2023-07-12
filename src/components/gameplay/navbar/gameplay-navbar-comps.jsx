f573fdd9536d98e57dc56e388744e542dbab0796
Hla Htun <kshap.karki@gmail.com>
2023-07-11 22:42:13 -0700

    hint component added in nav bar

--- src/components/gameplay/navbar/gameplay-navbar-comps.jsx
+++ src/components/gameplay/navbar/gameplay-navbar-comps.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import './gameplay-navbar-comps.css'
-// import LinkTo from "../../linkTo";
 import feedbackIcon from "../../../assets/feedbackIcon.png"
+import Hint from "./hint/gameplay-hint-comp";
 import LinkTo from "../../linkTo";
 
 const GameplayNavbar = () => {
     return (
         <div className="top-nav">
+            <Hint />
             <div className="logo">GuessiT</div>
             <div className="icons">
                 <img tabIndex={0} className="gameplay-navbar-feedback-button" id="feedbackButton" src={feedbackIcon} alt="Click to provide feedbacks for GuessIt"/>
