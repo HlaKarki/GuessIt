fbb3537b35dc12428c157fe6b174edcdf24dc9a9
Hla Htun <kshap.karki@gmail.com>
2023-07-12 22:41:58 -0700

    tapping the hint icon now displays the answer

--- src/components/gameplay/navbar/hint/gameplay-hint-comp.jsx
+++ src/components/gameplay/navbar/hint/gameplay-hint-comp.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './gameplay-hint-comp.css'
 import hintIcon from '../../../../assets/hintIcon.png'
+import HintMessage from "../../../alert-message/hint_message/hint_message";
 
-const Hint = () => {
+const Hint = ( {test_answer} ) => {
+    const [ showHint, setShowHint ] = useState(false)
+    const handleShowHint = () => {
+        setShowHint(!showHint)
+    }
+
     return (
-        <button className={"hint"}>
-            <img src={hintIcon} alt={"hint icon"} />
-        </button>
+        <div>
+            <button className={"hint"} onClick={() => handleShowHint()}>
+                <img src={hintIcon} alt={"hint icon"} />
+            </button>
+            {showHint && (
+                <>
+                    <HintMessage test_answer={test_answer}/>
+                    <div id={"modal-backdrop"}></div>
+                </>
+            )}
+        </div>
     )
 }
 
