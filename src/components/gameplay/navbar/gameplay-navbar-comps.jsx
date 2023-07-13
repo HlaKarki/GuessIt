19240b635756d267f774a70187dda9c39ea45be2
Hla Htun <kshap.karki@gmail.com>
2023-07-12 22:44:10 -0700

    calls getChosenWord to get the current word and renders it along with the Hint component

--- src/components/gameplay/navbar/gameplay-navbar-comps.jsx
+++ src/components/gameplay/navbar/gameplay-navbar-comps.jsx
@@ -1,13 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 import './gameplay-navbar-comps.css'
 import feedbackIcon from "../../../assets/feedbackIcon.png"
 import Hint from "./hint/gameplay-hint-comp";
 import LinkTo from "../../linkTo";
+import {getChosenWord} from "../gameplay_functions";
 
 const GameplayNavbar = () => {
+    const [chosen, setChosen] = useState("start")
+
+    getChosenWord
+        .then(chosenWord => {
+            setChosen(chosenWord.word)
+        })
+
     return (
         <div className="top-nav">
-            <Hint />
+            <Hint test_answer={chosen}/>
             <div className="logo">GuessiT</div>
             <div className="icons">
                 <img tabIndex={0} className="gameplay-navbar-feedback-button" id="feedbackButton" src={feedbackIcon} alt="Click to provide feedbacks for GuessIt"/>
