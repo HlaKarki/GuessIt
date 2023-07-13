2217a756ea976c40b57bcccb696a1c8e834c5423
Hla Htun <kshap.karki@gmail.com>
2023-07-12 23:58:40 -0700

    clicking on the hint icon now displays the word, definitions, 5 synonyms and 5 antonyms

--- src/components/alert-message/hint_message/hint_message.js
+++ src/components/alert-message/hint_message/hint_message.js
@@ -1,10 +1,30 @@
 // import './hint_message.css'
 import '../win_lose_message/win_lose_message.css'
 
-const HintMessage = ( {test_answer} ) => {
+const HintMessage = ( {word, definitions, synonyms, antonyms} ) => {
+    const MAX_SYNONYMS = 5;
+    const MAX_ANTONYMS = 5;
+
     return (
         <div className={"win_lose_message fade-in-animation"}>
-            {test_answer}
+            <div>{word}</div>
+            <div>{definitions}</div>
+            <div>Synonyms</div>
+            {synonyms !== undefined ? (
+                synonyms.slice(0, Math.min(MAX_SYNONYMS, synonyms.length)).map((syns, id) => (
+                    <div key={id}>{syns}</div>
+                ))
+            ) : (
+                <div>No synonyms available</div>
+            )}
+            <div>Antonyms</div>
+            {antonyms !== undefined ? (
+                antonyms.slice(0, Math.min(MAX_ANTONYMS, antonyms.length)).map((ants, id) => (
+                    <div key={id}>{ants}</div>
+                ))
+            ) : (
+                <div>No antonyms available</div>
+            )}
         </div>
     )
 }
