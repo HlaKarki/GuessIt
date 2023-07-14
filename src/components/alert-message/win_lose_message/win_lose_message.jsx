10651a94878bcc18d7f9e90b511e7773d992428a
Hla Htun <kshap.karki@gmail.com>
2023-07-13 23:09:59 -0700

    div class name changed to make use of alert_message general styling

--- src/components/alert-message/win_lose_message/win_lose_message.jsx
+++ src/components/alert-message/win_lose_message/win_lose_message.jsx
@@ -1,8 +1,9 @@
+import '../alert_message.css'
 import './win_lose_message.css'
 
 export const WinLoseMessage = ( {message_title} ) => {
     return (
-        <div className={"win_lose_message fade-in-animation"}>
+        <div className={"alert_message fade-in-animation"}>
             {message_title}
             <div>
                 <button className={"no-button"}>No</button>
