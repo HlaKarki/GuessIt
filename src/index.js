b06452c655f830b0782061c5bd855707ab82c2b4
Hla Htun <kshap.karki@gmail.com>
2023-08-01 20:20:14 -0700

    importing and using pointsprovider to wrap the whole app in it

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
-
 import App from './App'
 import './index.css'
+import { PointsProvider } from "./PointsContext";
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+    <PointsProvider>
+        <App />
+    </PointsProvider>
+);
