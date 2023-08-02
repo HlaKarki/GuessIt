519a76f6f36fcb510c83f5c337623b702dac7208
Hla Htun <kshap.karki@gmail.com>
2023-08-01 20:20:40 -0700

    custom hook; similar to useState, for the points variable to be globally accessible

---
+++ src/PointsContext.js
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+
+const PointsContext = createContext();
+
+export const usePoints = () => {
+    return useContext(PointsContext);
+};
+
+export const PointsProvider = ({ children }) => {
+    const [points, setPoints] = useState(10);
+
+    return (
+        <PointsContext.Provider value={[points, setPoints]}>
+            {children}
+        </PointsContext.Provider>
+    );
+};
